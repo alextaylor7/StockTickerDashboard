c8e5a1ee12d2910c747e2e40578d5027ba618d4a
alextaylor7 <43300702+alextaylor7@users.noreply.github.com>
2025-03-20 16:52:32 -0300

    Store the session stock prices

--- callbacks/dashboard_callbacks.py
+++ callbacks/dashboard_callbacks.py
@@ -1,12 +1,11 @@
 import dash
-from dash import Input, Output, callback
+from dash import Input, Output, callback, State
 import random
 import plotly.graph_objects as go
 from constants import commodities
 
-# Stock data setup
+# Initialize stock prices with default values
 stock_prices = {commodity: 1.00 for commodity in commodities}
-
 
 def roll_dice():
     stock = random.choice(commodities)
@@ -14,19 +13,21 @@ def roll_dice():
     value = random.choice([0.05, 0.10, 0.20])
     return stock, action, value
 
-
 @callback(
     [Output("stock-table", "data"),
      Output("rolled-stock", "children"),
      Output("rolled-action", "children"),
      Output("rolled-value", "children"),
      Output("stock-graph", "figure")],
-    Input("roll-btn", "n_clicks")
+    [Input("roll-btn", "n_clicks"),
+    Input("_initial_load", "data")],
+    prevent_initial_call=False
 )
-def update_stock(n):
+def update_stock(n, initial_load):
+    global stock_prices  # Move global declaration to the beginning of the function
+    
     if n > 0:
         stock, action, value = roll_dice()
-        global stock_prices
 
         if action == "Up":
             stock_prices[stock] += value
@@ -38,6 +39,9 @@ def update_stock(n):
         elif stock_prices[stock] == 0:
             stock_prices[stock] = 1.00
 
+        # Store updated stock prices in session
+        dash.get_app().server.config['STOCK_PRICES'] = stock_prices
+
         fig = go.Figure()
         fig.add_trace(go.Bar(x=list(stock_prices.keys()), y=list(stock_prices.values()), marker_color='blue'))
         fig.update_layout(yaxis=dict(range=[0, 2]), title="Stock Prices")
@@ -46,6 +50,21 @@ def update_stock(n):
                 f"Stock: {stock}",
                 f"Action: {action}",
                 f"Value: {value}",
+                fig)
+    elif initial_load:
+        # Get stored prices if they exist, otherwise use defaults
+        stored_prices = dash.get_app().server.config.get('STOCK_PRICES')
+        if stored_prices:
+            stock_prices = stored_prices
+
+        fig = go.Figure()
+        fig.add_trace(go.Bar(x=list(stock_prices.keys()), y=list(stock_prices.values()), marker_color='blue'))
+        fig.update_layout(yaxis=dict(range=[0, 2]), title="Stock Prices")
+
+        return ([{"Commodity": k, "Price": v} for k, v in stock_prices.items()],
+                "Stock: ",
+                "Action: ",
+                "Value: ",
                 fig)
 
     return dash.no_update
